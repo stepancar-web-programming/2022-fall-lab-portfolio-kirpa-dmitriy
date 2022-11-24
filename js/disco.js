6e0adbce620fbc9ca669679bcaca67e272ceb33c
Milksisis <106484677+Milksisis@users.noreply.github.com>
2022-11-24 12:36:37 +0300

    Update js/disco.js
    Co-authored-by: Stepan Mikhailiuk <istepancar@gmail.com>

--- js/disco.js
+++ js/disco.js
@@ -1,7 +1,6 @@
 const button = document.getElementById('button')
 
 button.addEventListener('click', () => {
-  let Color = '#'
-  Color += Math.random().toString(16).slice(2, 8)
+  const color += Math.random().toString(16).slice(2, 8)
   document.body.style.backgroundColor = Color
 })
