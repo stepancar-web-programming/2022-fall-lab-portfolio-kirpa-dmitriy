4f0176cb6013e424943f1bdc0f21f40121c7e3ee
maximka65 <maxim652002@gmail.com>
2022-11-26 14:49:02 +0300

    3

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,27 @@
 module.exports = {
   env: {
     browser: true,
+    commonjs: true,
     es2021: true
   },
   extends: 'airbnb-base',
   overrides: [],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+    ecmaVersion: 'latest'
   },
   rules: {
-    'linebreak-style': process.env.NODE_ENV === 'prod' ? 'unix' : 'windows',
+    'linebreak-style': 0,
     'no-param-reassign': 0,
     'no-unused-expressions': 0,
     'no-unused-vars': 0,
-    camelcase: 0,
+    // camelcase: 0,
     'no-undef': 0,
     semi: 0,
     'no-plusplus': 0,
     'func-names': 0,
     'no-console': 0,
     'default-case': 0,
-    'no-multi-assign': 0
+    'no-multi-assign': 0,
+    'comma-dangle': 0
   }
 }
--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,17 +1,17 @@
 $(document).ready(() => {
-  const all_panels = $('.portfolio-accordion > li > ul').hide()
-  $('.portfolio-accordion > li > a').click(function () {
+  const allPanels = $('.portfolio-accordion > li > ul').hide()
+  $('.portfolio-accordion > li > a').click(function (a) {
     const target = $(this).next()
     if (!target.hasClass('active')) {
-      all_panels.removeClass('active').slideUp()
+      allPanels.removeClass('active').slideUp()
       target.addClass('active').slideDown()
     }
     return false
   })
 
   $('.product-links-wap a').click(function () {
-    const this_src = $(this).children('img').attr('src')
-    $('#product-detail').attr('src', this_src)
+    const thisSrc = $(this).children('img').attr('src')
+    $('#product-detail').attr('src', thisSrc)
     return false
   })
 
@@ -32,8 +32,8 @@ $(document).ready(() => {
   })
 
   $('.btn-size').click(function () {
-    const this_val = $(this).html()
-    $('#product-size').val(this_val)
+    const thisVal = $(this).html()
+    $('#product-size').val(thisVal)
     $('.btn-size').removeClass('btn-secondary')
     $('.btn-size').addClass('btn-success')
     $(this).removeClass('btn-success')
