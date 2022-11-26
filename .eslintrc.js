9fdf2091ca7e50a6fab7a11379965367845202e5
maximka65 <maxim652002@gmail.com>
2022-11-26 12:12:14 +0300

    up

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,20 +4,24 @@ module.exports = {
     es2021: true,
   },
   extends: 'airbnb-base',
-  overrides: [],
+  overrides: [
+  ],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
   rules: {
-    'linebreak-style': 0,
+    'linebreak-style': process.env.NODE_ENV === 'prod' ? 'unix' : 'windows',
     'no-param-reassign': 0,
     'no-unused-expressions': 0,
     'no-unused-vars': 0,
-    'no-plusplus': 0,
-    'camelcase': 0,
+    camelcase: 0,
     'no-undef': 0,
-    'semi': 0,
+    semi: 0,
     'no-plusplus': 0,
+    'func-names': 0,
+    'no-console': 0,
+    'default-case': 0,
+    'no-multi-assign': 0,
   },
-};
+}
