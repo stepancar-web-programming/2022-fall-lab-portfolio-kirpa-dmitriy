eb7689487ff36c12a76acca3557102edc1516343
maximka65 <maxim652002@gmail.com>
2022-11-26 12:45:44 +0300

    1

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,8 +1,9 @@
 module.exports = {
-    extends: ["stylelint-config-standard",
-        "stylelint-config-rational-order",
-        "stylelint-prettier/recommended"],
-    plugins: ["stylelint-order", "stylelint-scss"],
-    rules: {
-    }
-};
+    extends: [
+        'stylelint-config-standard',
+        'stylelint-config-rational-order',
+        'stylelint-prettier/recommended'
+    ],
+    plugins: ['stylelint-order', 'stylelint-scss'],
+    rules: {}
+}
